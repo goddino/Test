6a5b8768d8246da7e580f1cc298a03fafdadebab
Xiangrong <goddino@gmail.com>
2013-12-06 21:57:12 +0800

    To switch Android compatible socket.io library. Other computers now able to connect to ws webserver (http://49.128.39.190:8887) after doing public IP port mapping to internal ip for big Mac (192.168.1.113:8887). However, Android was still unable to connect to webserver despite successfully compiling and running two different Android WebSocket libraries (Java-WebSocket and AutobahnAndroid). This was likely due to the fact that socket.io has an additional handshake on top of WebSocket protocol, hence only Android (compatible) library for socket.io (not just WebSocket) should be used for Android. Ref: http://nkzawa.tumblr.com/post/46850605422/connecting-to-a-socket-io-server-from-android After much online searching (esp. on stackoverflow), it seemed the most popular choice is socket.io-java-client by Gottox at: https://github.com/Gottox/socket.io-java-client To try out socket.io-java-client.

--- WS-Android-Example/AndroidWsChat/src/com/test/androidwschat/MainActivity.java
+++ WS-Android-Example/AndroidWsChat/src/com/test/androidwschat/MainActivity.java
@@ -1,29 +1,19 @@
 package com.test.androidwschat;
 
-import com.test.androidwschat.R;
-
-import android.os.Bundle;
-import org.java_websocket.client.*;
-
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-// For Java-WebSocket
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_10;
-import org.java_websocket.drafts.Draft_17;
-import org.java_websocket.framing.Framedata;
-import org.java_websocket.handshake.ServerHandshake;
+import de.tavendo.autobahn.WebSocketConnection;
+import de.tavendo.autobahn.WebSocketException;
+import de.tavendo.autobahn.WebSocketHandler;
 
 
+// public class MainActivity extends Activity {
 public class MainActivity extends Activity {
 
   // Instantiate views
@@ -31,24 +21,18 @@ public class MainActivity extends Activity {
   private EditText edtVwUserMsg;
   private Button btnSendChat;
   
-  // Instantiate Java-WebSocket WebSocketClient;
-  private WsClient wsClient;
-    
+  private static String TAG;
+ 
+  // Instantiate Autobahn WebSocket Connection;
+  private WebSocketConnection mConnection;
+ 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    
-    // Initialise wsClient
-    try {
-      // wsClient = new WsClient( new URI( "ws://localhost:8887" ), new Draft_17() );
-      // wsClient = new WsClient( new URI( "ws://49.128.39.190:8887" ), new Draft_17() );
-      wsClient = new WsClient( new URI( "ws://192.168.1.113:8887" ), new Draft_17() );
-    } catch (URISyntaxException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
-    wsClient.connect();
+    TAG = "MainActivity";
+    mConnection = new WebSocketConnection();
+    start();
     
     // Set Views
     txtVwChatMsgs = ( TextView ) findViewById( R.id.textViewChatMessages );
@@ -62,18 +46,22 @@ public class MainActivity extends Activity {
           // Example of socket.io message:
           // {"name":"MessageName",
           // "args":[{"ObjectKey1":"Key1Value","ObjectKey2":"Key2Value"}]}
-      String msg = "{\"name\":\"msgServer\"," +
+          // {"name":"msgServer","args":[{"username":"bm","message":"hello"}]}
+      String msgS = "{\"name\":\"msgServer\"," +
+                   "\"args\":[" + 
+                    "{\"username\":\"Android\"," +
+                    "\"message\":\"Test Android.\"}]}";
+      String msgC = "{\"name\":\"msgClient\"," +
                    "\"args\":[" + 
                     "{\"username\":\"Android\"," +
                     "\"message\":\"Test Android.\"}]}";
 
         
-        // Send via Java-WebSocket client.
-        wsClient.send( msg );
+        // Send via Autobahn connection.
+        mConnection.sendTextMessage( msgS );
+        mConnection.sendTextMessage( msgC );
       }
     });
-    
-
   }
 
   @Override
@@ -83,42 +71,34 @@ public class MainActivity extends Activity {
     return true;
   }
 
-  public class WsClient extends WebSocketClient {
+  private void start() {
+ 
+    final String wsuri = "ws://49.128.39.190:8887";
 
-    public WsClient( URI serverUri , Draft draft ) {
-      super( serverUri, draft );
+    try {
+      mConnection.connect(wsuri, new WebSocketHandler() {
+
+        @Override
+        public void onOpen() {
+           Log.d(TAG, "Status: Connected to " + wsuri);
+           mConnection.sendTextMessage("Hello, world!");
+        }
+
+        @Override
+        public void onTextMessage(String payload) {
+           Log.d(TAG, "Got echo: " + payload);
+           String existingChat = (String) txtVwChatMsgs.getText();
+           txtVwChatMsgs.setText( existingChat + "\n" + payload );
+        }
+
+        @Override
+        public void onClose(int code, String reason) {
+           Log.d(TAG, "Connection lost.");
+        }
+      });
+    } catch (WebSocketException e) {
+
+      Log.d(TAG, e.toString());
     }
-
-    public WsClient( URI serverURI ) {
-      super( serverURI );
-    }
-
-    @Override
-    public void onOpen( ServerHandshake handshakedata ) {
-      System.out.println( "opened connection" );
-      // if you plan to refuse connection based on ip or httpfields overload: onWebsocketHandshakeReceivedAsClient
-    }
-
-    @Override
-    public void onMessage( String message ) {
-      System.out.println( "received: " + message );
-    }
-
-    @Override
-    public void onFragment( Framedata fragment ) {
-      System.out.println( "received fragment: " + new String( fragment.getPayloadData().array() ) );
-    }
-
-    @Override
-    public void onClose( int code, String reason, boolean remote ) {
-      // The codecodes are documented in class org.java_websocket.framing.CloseFrame
-      System.out.println( "Connection closed by " + ( remote ? "remote peer" : "us" ) );
-    }
-
-    @Override
-    public void onError( Exception ex ) {
-      ex.printStackTrace();
-      // if the error is fatal then onClose will be called additionally
-    }    
-  }  
+  }
 }
---
+++ WS-Android-Example/Server/bigMac-Ubuntu/public/chat.js
@@ -0,0 +1,44 @@
+// On complete loading, assign functionality to front end elements.
+window.onload = function() {
+ 
+  var messages = [];
+  // var socket = io.connect('http://localhost:8887');
+  // var socket = io.connect('ws://localhost:8887');
+  var socket = io.connect('ws://49.128.39.190:8887/');
+  var username = document.getElementById("username");
+  var field = document.getElementById("msgField");
+  var sendButton = document.getElementById("sendButton");
+  var content = document.getElementById("content");
+
+  socket.on('msgServer', function (data) {
+    if(data.message) {
+      messages.push(data);
+      // var html = '';
+      var html = 'Raw data:' + '<br />' + data + '<br />';
+      for(var i=0; i<messages.length; i++) {
+          html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+          html += messages[i].message + '<br />';
+      }
+      content.innerHTML = html;
+    } else {
+      console.log("There is a problem:", data);
+    }
+  });
+
+  sendChat = function() {
+    if(username.value == "") {
+      alert("Please type your name!");
+    } else {
+      var text = field.value;
+      socket.emit('msgClient', { username: username.value, message: text });
+    }
+  };
+
+  field.onkeyup = function( event ) {
+    if( event.keyCode == 13 ) {
+      sendChat();
+    }
+  }
+  
+  sendButton.onclick = sendChat;
+}
